7cf556541ef1ee4a2b61af4d9d2cb1de1883aebf
Sankar-web-dev <Sankar@webzenith.tech>
2026-01-27 22:46:48 +0530

    fix workflow 7

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 
 
-export const supabase = createClient<any>(
+export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 );
